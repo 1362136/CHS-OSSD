d46d384f9f777d08864930c20a93be180beeb9b5
Ahmad-Bamba <ahmad.eb.bamba@gmail.com>
2014-01-12 13:14:46 -0500

    Added the form returning error:entry not defined, will fix

--- Research Helper++/Research Helper++/Research Helper++.h
+++ Research Helper++/Research Helper++/Research Helper++.h
@@ -54,7 +54,7 @@ namespace ResearchHelper {
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(943, 581);
+			this->ClientSize = System::Drawing::Size(1105, 636);
 			this->Name = L"ResearchHelper";
 			this->Text = L"ResearchHelper";
 			this->ResumeLayout(false);
