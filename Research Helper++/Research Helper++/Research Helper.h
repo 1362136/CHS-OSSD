f8a009b8a17f9829646b1cc7723f3679315db259
Ahmad-Bamba <ahmad.eb.bamba@gmail.com>
2014-01-20 11:01:21 -0500

    FINALLY FIX EVERYTHIN :D It didn't like the custom icon? Anyway first stable version

--- Research Helper++/Research Helper++/Research Helper.h
+++ Research Helper++/Research Helper++/Research Helper.h
@@ -68,18 +68,17 @@ namespace ResearchHelperSpace {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(ResearchHelper::typeid));
 			this->tabControl = (gcnew System::Windows::Forms::TabControl());
+			this->EDDTab = (gcnew System::Windows::Forms::TabPage());
 			this->LogTab = (gcnew System::Windows::Forms::TabPage());
 			this->NotesTabs = (gcnew System::Windows::Forms::TabPage());
-			this->EDDTab = (gcnew System::Windows::Forms::TabPage());
 			this->DataTab = (gcnew System::Windows::Forms::TabPage());
 			this->SearchTab = (gcnew System::Windows::Forms::TabPage());
-			this->webBrowser1 = (gcnew System::Windows::Forms::WebBrowser());
-			this->Backwards_btn = (gcnew System::Windows::Forms::Button());
-			this->Forward_btn = (gcnew System::Windows::Forms::Button());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->Go_btn = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->Forward_btn = (gcnew System::Windows::Forms::Button());
+			this->Backwards_btn = (gcnew System::Windows::Forms::Button());
+			this->webBrowser1 = (gcnew System::Windows::Forms::WebBrowser());
 			this->tabControl->SuspendLayout();
 			this->SearchTab->SuspendLayout();
 			this->SuspendLayout();
@@ -96,6 +95,16 @@ namespace ResearchHelperSpace {
 			this->tabControl->SelectedIndex = 0;
 			this->tabControl->Size = System::Drawing::Size(1353, 722);
 			this->tabControl->TabIndex = 0;
+			// 
+			// EDDTab
+			// 
+			this->EDDTab->Location = System::Drawing::Point(4, 22);
+			this->EDDTab->Name = L"EDDTab";
+			this->EDDTab->Padding = System::Windows::Forms::Padding(3);
+			this->EDDTab->Size = System::Drawing::Size(1345, 696);
+			this->EDDTab->TabIndex = 2;
+			this->EDDTab->Text = L"EDD";
+			this->EDDTab->UseVisualStyleBackColor = true;
 			// 
 			// LogTab
 			// 
@@ -116,16 +125,6 @@ namespace ResearchHelperSpace {
 			this->NotesTabs->TabIndex = 1;
 			this->NotesTabs->Text = L"Notes";
 			this->NotesTabs->UseVisualStyleBackColor = true;
-			// 
-			// EDDTab
-			// 
-			this->EDDTab->Location = System::Drawing::Point(4, 22);
-			this->EDDTab->Name = L"EDDTab";
-			this->EDDTab->Padding = System::Windows::Forms::Padding(3);
-			this->EDDTab->Size = System::Drawing::Size(1345, 696);
-			this->EDDTab->TabIndex = 2;
-			this->EDDTab->Text = L"EDD";
-			this->EDDTab->UseVisualStyleBackColor = true;
 			// 
 			// DataTab
 			// 
@@ -152,23 +151,21 @@ namespace ResearchHelperSpace {
 			this->SearchTab->Text = L"Search Web";
 			this->SearchTab->UseVisualStyleBackColor = true;
 			// 
-			// webBrowser1
+			// Go_btn
 			// 
-			this->webBrowser1->Location = System::Drawing::Point(3, 35);
-			this->webBrowser1->MinimumSize = System::Drawing::Size(20, 20);
-			this->webBrowser1->Name = L"webBrowser1";
-			this->webBrowser1->Size = System::Drawing::Size(1339, 661);
-			this->webBrowser1->TabIndex = 0;
-			this->webBrowser1->Url = (gcnew System::Uri(L"http://www.jstor.org/", System::UriKind::Absolute));
+			this->Go_btn->Location = System::Drawing::Point(1269, 6);
+			this->Go_btn->Name = L"Go_btn";
+			this->Go_btn->Size = System::Drawing::Size(70, 23);
+			this->Go_btn->TabIndex = 4;
+			this->Go_btn->Text = L"Go";
+			this->Go_btn->UseVisualStyleBackColor = true;
 			// 
-			// Backwards_btn
+			// textBox1
 			// 
-			this->Backwards_btn->Location = System::Drawing::Point(6, 6);
-			this->Backwards_btn->Name = L"Backwards_btn";
-			this->Backwards_btn->Size = System::Drawing::Size(75, 23);
-			this->Backwards_btn->TabIndex = 1;
-			this->Backwards_btn->Text = L"Backwards";
-			this->Backwards_btn->UseVisualStyleBackColor = true;
+			this->textBox1->Location = System::Drawing::Point(169, 8);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(1094, 20);
+			this->textBox1->TabIndex = 3;
 			// 
 			// Forward_btn
 			// 
@@ -179,21 +176,23 @@ namespace ResearchHelperSpace {
 			this->Forward_btn->Text = L"Forward";
 			this->Forward_btn->UseVisualStyleBackColor = true;
 			// 
-			// textBox1
+			// Backwards_btn
 			// 
-			this->textBox1->Location = System::Drawing::Point(169, 8);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(1094, 20);
-			this->textBox1->TabIndex = 3;
+			this->Backwards_btn->Location = System::Drawing::Point(6, 6);
+			this->Backwards_btn->Name = L"Backwards_btn";
+			this->Backwards_btn->Size = System::Drawing::Size(75, 23);
+			this->Backwards_btn->TabIndex = 1;
+			this->Backwards_btn->Text = L"Backwards";
+			this->Backwards_btn->UseVisualStyleBackColor = true;
 			// 
-			// Go_btn
+			// webBrowser1
 			// 
-			this->Go_btn->Location = System::Drawing::Point(1269, 6);
-			this->Go_btn->Name = L"Go_btn";
-			this->Go_btn->Size = System::Drawing::Size(70, 23);
-			this->Go_btn->TabIndex = 4;
-			this->Go_btn->Text = L"Go";
-			this->Go_btn->UseVisualStyleBackColor = true;
+			this->webBrowser1->Location = System::Drawing::Point(3, 35);
+			this->webBrowser1->MinimumSize = System::Drawing::Size(20, 20);
+			this->webBrowser1->Name = L"webBrowser1";
+			this->webBrowser1->Size = System::Drawing::Size(1339, 661);
+			this->webBrowser1->TabIndex = 0;
+			this->webBrowser1->Url = (gcnew System::Uri(L"http://www.jstor.org/", System::UriKind::Absolute));
 			// 
 			// ResearchHelper
 			// 
@@ -201,7 +200,6 @@ namespace ResearchHelperSpace {
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1377, 746);
 			this->Controls->Add(this->tabControl);
-			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
 			this->Name = L"ResearchHelper";
 			this->Text = L"ResearchHelper";
 			this->Load += gcnew System::EventHandler(this, &ResearchHelper::ResearchHelper_Load);
@@ -217,3 +215,4 @@ namespace ResearchHelperSpace {
 	private: System::Void ResearchHelper_Load(System::Object^  sender, System::EventArgs^  e);
 	};//end of class ResearchHelper
 }
+//end custom namespace
